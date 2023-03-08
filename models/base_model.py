d67fd7930db57d3c55499db577c1e3ce38116734
JoshAmpofo <joshampofo92@gmail.com>
2023-03-08 11:01:47 +0000

    Update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python3
 """ Define main model class for console"""
+import cmd
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+    prompt = '(hbnb) '
+
+    def __init__(self):
+        """initialize BaseModel Instances"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """saves object instance modification date"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns dictionary copy of all instances"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def __str__(self):
+        """informal string representation of class objects"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
