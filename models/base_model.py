5937993a3f0b758a06d1862710ea4ea957dab05c
JoshAmpofo <joshampofo92@gmail.com>
2023-03-08 12:30:26 +0000

    Correct datetime

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
         """initialize BaseModel Instances"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
     def save(self):
         """saves object instance modification date"""
