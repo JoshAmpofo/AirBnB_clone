f064f5751946d90f1e58ec8f708d3480fdb84ade
JoshAmpofo <joshampofo92@gmail.com>
2023-03-09 20:08:25 +0000

    Update import modules

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,8 +2,14 @@
 """Define a file storage class for BaseModel object storage"""
 
 import json
+
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
