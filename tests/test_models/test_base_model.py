821301cc9ae8013f014e61351a73bb05363fe142
JoshAmpofo <joshampofo92@gmail.com>
2023-03-09 21:12:08 +0000

    Fix PEP-8 indentation error

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""Unittest cases for base_model.py
-Unittest classes:
-    TestBaseModel - line 16
-"""
+"""Unittest cases for base_model.py"""
 
 import os
 import unittest
@@ -19,33 +16,33 @@ class TestBaseModel(unittest.TestCase):
         """test for instance id assignment on initialization"""
         bmd = BaseModel()
         self.assertTrue(hasattr(bmd, 'id'))
-    
+
     def test_unique_id(self):
         """test if id instances are unique of each other"""
         bmd_1 = BaseModel()
         bmd_2 = BaseModel()
         self.assertNotEqual(bmd_1.id, bmd_2.id)
-    
+
     def test_id_str(self):
         """check if id generated is a str"""
         bmd = BaseModel()
         self.assertTrue(type(bmd.id) is str)
-    
+
     def test_created_at_datetime(self):
         """check if created_at is a datetime"""
         bmd = BaseModel()
         self.assertTrue(type(bmd.created_at) is datetime)
-    
+
     def test_updated_at_datetime(self):
         """check if updated_at is a datetime"""
         bmd = BaseModel()
         self.assertTrue(type(bmd.updated_at) is datetime)
-    
+
     def test_created_at_equal_updated_at_datetime(self):
         """check if initial datetime of created_at == datetime of updated at"""
         bmd = BaseModel()
         self.assertEqual(bmd.created_at, bmd.updated_at)
-    
+
     def test_save(self):
         """check if update time is different from previous time"""
         bmd = BaseModel()
@@ -54,35 +51,35 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(bmd.updated_at, bmd.created_at)
         self.assertGreater(bmd.updated_at.microsecond,
                            bmd.created_at.microsecond)
-    
+
     def test_str_format(self):
         """check if str rep is correct"""
         bmd = BaseModel()
-        self.assertEqual(str(bmd), 
-                "[BaseModel] ({}) {}".format(bmd.id, bmd.__dict__))
-    
+        self.assertEqual(str(bmd),
+                         "[BaseModel] ({}) {}".format(bmd.id, bmd.__dict__))
+
     def test_to_dict(self):
         """check if to_dict is dictionary"""
         bmd = BaseModel()
         self.assertTrue(type(bmd.to_dict()) is dict)
-    
+
     def test_to_dict_class_dunder(self):
         """checks if BaseModel.to_dict() contains __class__"""
         bmd = BaseModel()
         self.assertTrue("__class__" in bmd.to_dict())
-    
+
     def test_created_at_datetime_isoformat(self):
         """checks if datetime format of created_at is ISO"""
         bmd = BaseModel()
-        self.assertEqual(bmd.to_dict()["created_at"], 
-                bmd.created_at.isoformat())
-    
+        self.assertEqual(bmd.to_dict()["created_at"],
+                         bmd.created_at.isoformat())
+
     def test_updated_at_datetime_isoformat(self):
         """checks if datetime format of created_at is ISO"""
         bmd = BaseModel()
-        self.assertEqual(bmd.to_dict()["updated_at"], 
-                bmd.updated_at.isoformat())
-    
+        self.assertEqual(bmd.to_dict()["updated_at"],
+                         bmd.updated_at.isoformat())
+
     def test_no_args_usage(self):
         """checks if *args is not used in BaseModel dictionary"""
         bmd = BaseModel(None)
@@ -107,7 +104,7 @@ class TestBaseModel(unittest.TestCase):
                                            "%Y-%m-%dT%H:%M:%S.%f"))
 
     def test_kwargs_empty(self):
-        """checks if id, created_at and update_at are generated 
+        """checks if id, created_at and update_at are generated
            automatically if not in kwargs
         """
         obj_dict = {}
@@ -126,15 +123,15 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(bmd.name, "AirBnB")
 
     def test_dict_obj_to_BaseModel(self):
-        """checks if storage.new() is not called when a BaseModel obj is 
+        """checks if storage.new() is not called when a BaseModel obj is
            created from a dict obj
         """
-        obj_dict = {"id":uuid4(), "created_at":datetime.utcnow().isoformat(), 
-                   "updated_at":datetime.utcnow().isoformat(), 
-                   "name":"AirBnB"}
+        obj_dict = {"id": uuid4(), "created_at": datetime.utcnow().isoformat(),
+                    "updated_at": datetime.utcnow().isoformat(),
+                    "name": "AirBnB"}
         bmd = BaseModel(**obj_dict)
         self.assertNotIn(bmd, models.storage.all().values())
-        
+
         del bmd
 
         bmd = BaseModel()
