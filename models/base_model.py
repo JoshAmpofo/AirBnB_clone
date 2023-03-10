ab6ad8c1479f61d6dca0e0a09b1053791df640dc
JoshAmpofo <joshampofo92@gmail.com>
2023-03-10 16:41:05 +0000

    Fixing bugs

--- models/base_model.py
+++ models/base_model.py
@@ -22,6 +22,11 @@ class BaseModel:
             self.created_at = self.updated_at = datetime.now()
             storage.new(self)
 
+    def __str__(self):
+        """informal string representation of class objects"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+
     def save(self):
         """saves object instance modification date"""
         from models import storage
@@ -31,12 +36,7 @@ class BaseModel:
     def to_dict(self):
         """returns dictionary copy of all instances"""
         obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
         return obj_dict
-
-    def __str__(self):
-        """informal string representation of class objects"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                     self.__dict__)
